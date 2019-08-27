78a1a53a262ec20eebc3dfbd6339b4cd37b36f6c
Remsh <3377254329@qq.com>
2019-08-27 09:57:18 +0800

    Update main.go
    add hostname

--- hello/main.go
+++ hello/main.go
@@ -4,10 +4,17 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Hello, 世界")
+	name, err := os.Hostname()
+	if err != nil {
+		panic(err)
+	}
+	
+	fmt.Println("hostname:", name)
 }
 
 func main() {
